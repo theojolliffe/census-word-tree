1b1c22ae3e3d635469ff778ac380c7a1263fa41e
Theo Jolliffe <theojolliffe@gmail.com>
2022-09-12 15:54:05 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<div style="width: 80%; margin:auto;">
+	<h1>
+		Census Area Reports Word Tree
+	</h1>
+	<p>
+		Click on a phrase type (e.g. 'clausal modifier') to see the various phrases used across different topics.
+	</p>
+	<p>
+		Click on a phrase to see it used in context. 
+	</p>
+	<p>
+		Note that when a phrase is selected, only the bold text is dynamic so may not provide perfect context.
+	</p>
+</div>
+
+<iframe width="100%" height="5212" frameborder="0"
+  src="https://observablehq.com/embed/cc9449078bb7b9ae?cells=chart%2Csent">
+</iframe>  
+<style>
+	.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
+</style>
